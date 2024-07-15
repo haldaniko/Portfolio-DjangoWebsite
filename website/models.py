84a9d322cd97359a8a97ad60f95ad301ee084518
haldaniko <haldaniko@gmail.com>
2024-07-15 13:27:59 +0200

    add models for website content

--- website/models.py
+++ website/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    lvl = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class WorkExpirience(models.Model):
+    position_name = models.CharField(max_length=255)
+    company_name = models.CharField(max_length=255)
+    company_link = models.CharField(blank=True, null=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    location = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.position_name} in {self.company_name}"
+
+
+class StudyExpirience(models.Model):
+    school_name = models.CharField(max_length=255)
+    school_link = models.CharField(max_length=255)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    location = models.CharField(max_length=255)
+    descritpion = models.TextField()
+
+    def __str__(self):
+        return f"{self.school_name}"
+
+
+class PortfolioExample(models.Model):
+    name = models.CharField(max_length=255)
+    description_small = models.CharField(max_length=255)
+    description_photo = models.ImageField()
+    project_photo = models.ImageField()
+    about = models.TextField()
+    github_source = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Certificate(models.Model):
+    name = models.CharField(max_length=255)
+    credential_link = models.CharField(max_length=255)
+    photo = models.ImageField()
+
+    def __str__(self):
+        return self.name
