58f7bbaf9adf54b55bb01078be26ee791767fd87
haldaniko <haldaniko@gmail.com>
2024-07-18 22:45:02 +0200

    create view and urls for main page

---
+++ website/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from website.views import index
+
+app_name = "website"
+
+urlpatterns = [
+    path('index/', index, name='index'),
+]
--- website/views.py
+++ website/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    # num_topics = Topic.objects.count()
+    # num_redactors = Redactor.objects.count()
+    # num_newspapers = Newspaper.objects.count()
+    #
+    # context = {
+    #     "num_topics": 1,
+    #     "num_redactors": 2,
+    #     "num_newspapers": 3,
+    # }
+
+    return render(request, "base.html")
