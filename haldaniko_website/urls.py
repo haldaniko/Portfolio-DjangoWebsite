0bf40977e4443bbc7dd852379dec4441a606872e
haldaniko <haldaniko@gmail.com>
2024-07-18 22:45:42 +0200

    include website.urls

--- haldaniko_website/urls.py
+++ haldaniko_website/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include("website.urls", namespace="website")),
 ]
+# + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
