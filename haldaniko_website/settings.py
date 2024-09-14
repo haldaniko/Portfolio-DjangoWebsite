08a13535ca9d170efa42fae41b08ed0f9299f0d7
haldaniko <haldaniko@gmail.com>
2024-09-14 16:27:52 +0200

    add MERIA_URL and MEDIA_ROOT

--- haldaniko_website/settings.py
+++ haldaniko_website/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,4 +83,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
