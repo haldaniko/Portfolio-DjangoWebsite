e6692eb31faed3131badd5e22954cb7826b24110
haldaniko <haldaniko@gmail.com>
2024-09-14 16:31:21 +0200

    redefine upload links for 'certif_file_path' and 'portfolio_file_path'

--- website/models.py
+++ website/models.py
@@ -10,14 +10,14 @@ def certif_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join(f"uploads/certificates/", filename)
+    return os.path.join(f"certificates/", filename)
 
 
 def portfolio_file_path(instance, filename):
     _, extension = os.path.splitext(filename)
     filename = f"{slugify(instance.name)}-{uuid.uuid4()}{extension}"
 
-    return os.path.join(f"uploads/portfolio/", filename)
+    return os.path.join(f"portfolio-content/", filename)
 
 
 class Skill(models.Model):
