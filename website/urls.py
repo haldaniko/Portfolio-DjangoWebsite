a19336247e306634cfbc32c930fb87fe6b50f3fa
haldaniko <haldaniko@gmail.com>
2024-09-14 16:31:45 +0200

    delete unnecessary endpoint

--- website/urls.py
+++ website/urls.py
@@ -1,14 +1,20 @@
 from django.urls import path
-from website.views import index, resume, PortfolioExampleListView, CertificateListView, contact
+from website.views import (
+    index,
+    resume,
+    contact,
+    PortfolioExampleListView,
+    CertificateListView,
+    PortfolioExampleDetailView
+)
 
 app_name = "website"
 
 urlpatterns = [
-    path('index/', index, name='index'),
-    path("home/", index, name="index"),
     path("", index, name='index'),
     path("resume/", resume, name="resume"),
     path("portfolio/", PortfolioExampleListView.as_view(), name="portfolio"),
+    path('portfolio/<int:pk>/', PortfolioExampleDetailView.as_view(), name='portfolio_detail'),
     path("certificates/", CertificateListView.as_view(), name="certificates"),
     path("contact/", contact, name="contact")
 ]
