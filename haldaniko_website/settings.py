716eb0518ba9832be6a700c32f2d0f8ce57acfec
haldaniko <haldaniko@gmail.com>
2024-09-29 22:53:43 +0200

    switch Debug on False

--- haldaniko_website/settings.py
+++ haldaniko_website/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['haldaniko.com.ua', 'www.haldaniko.com.ua']
 
